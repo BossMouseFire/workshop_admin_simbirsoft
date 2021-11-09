1767ac17199e19ffc04085e129ded85fd0da1868
Данил <abanin-02@mail.ru>
2021-11-10 01:39:16 +0400

    edit flexible layot

--- src/components/adminPage/menu/menu.tsx
+++ src/components/adminPage/menu/menu.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './menu.module.scss';
 import logo from '../../../assets/logo.svg';
 import { ReactComponent as Pen } from '../../../assets/pen.svg';
 import { ReactComponent as List } from '../../../assets/list.svg';
 import { ReactComponent as Order } from '../../../assets/order.svg';
+import cn from 'classnames';
 import Action from './action';
 export const Menu: React.FC = () => {
+  const [isActive, setActive] = useState<boolean>(false);
   return (
-    <div className={styles.menu}>
-      <div className={styles.head}>
-        <img src={logo} alt={'Логотип'} />
-        <span>Need for car</span>
+    <>
+      <div className={cn(styles.menu, { [styles.menuActive]: isActive })}>
+        <div className={styles.head}>
+          <img src={logo} alt={'Логотип'} />
+          <span>Need for car</span>
+        </div>
+        <Action>
+          <Pen />
+          <span>Карточка автомобиля</span>
+        </Action>
+        <Action>
+          <List />
+          <span>Список авто</span>
+        </Action>
+        <Action>
+          <Order />
+          <span>Заказы</span>
+        </Action>
       </div>
-      <Action>
-        <Pen />
-        <span>Карточка автомобиля</span>
-      </Action>
-      <Action>
-        <List />
-        <span>Список авто</span>
-      </Action>
-      <Action>
-        <Order />
-        <span>Заказы</span>
-      </Action>
-    </div>
+      <div
+        className={styles.buttonSetActiveMenu}
+        onClick={() => setActive((state) => !state)}
+      >
+        <span />
+      </div>
+    </>
   );
 };
